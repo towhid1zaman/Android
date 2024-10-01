28919a4a2101ad386fd3037fd5a8fe9ee3cf9d75
Towhid Zaman <towhid1zaman@gmail.com>
2024-10-02 00:44:06 +0600

    added login ui

--- IntroductionDemo/app/src/main/java/com/example/introductiondemo/MainActivity.java
+++ IntroductionDemo/app/src/main/java/com/example/introductiondemo/MainActivity.java
@@ -1,24 +1,30 @@
 package com.example.introductiondemo;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //interactivity through button
+    //an ui for login.
+    public void login(View view) {
+
+        EditText userNameEditText = findViewById(R.id.usernameEditText);
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
+
+        Log.i("Info", "Button Pressed");
+        Log.i("Username", userNameEditText.toString());
+        Log.i("passwordEditText", passwordEditText.toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
