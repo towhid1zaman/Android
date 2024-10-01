b49f6c6f5cc4caaf041cd261fad516903e181e05
Towhid Zaman <towhid1zaman@gmail.com>
2024-10-02 01:35:27 +0600

    add image

--- IntroductionDemo/app/src/main/java/com/example/introductiondemo/MainActivity.java
+++ IntroductionDemo/app/src/main/java/com/example/introductiondemo/MainActivity.java
@@ -4,26 +4,33 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private String tag = "MainActivity";
+    private final String tag = "MainActivity";
 
     //interactivity through button
     //an ui for login.
     public void login(View view) {
 
-        EditText userNameEditText = (EditText) findViewById(R.id.usernameEditText);
-        EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        EditText userNameEditText = findViewById(R.id.usernameEditText);
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
 
         Log.i(tag, "Button Pressed");
         Log.i(tag,"Username " + userNameEditText.getText().toString());
         Log.i(tag,"passwordEditText " +  passwordEditText.getText().toString());
 
-        // displaying text, its called toast.
-        Toast.makeText(this, "Hi there! Button Pressed", Toast.LENGTH_SHORT).show();
+        // show an image when button clicked and userName exist
+        if (!userNameEditText.getText().toString().isEmpty()) {
+            ImageView userImage = findViewById(R.id.userImage);
+            userImage.setImageResource(R.drawable.cat);
+        } else {
+            Toast.makeText(this, "Please fill up the information", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     @Override
