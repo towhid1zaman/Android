daf539474b85d225752b03c470746dedfc6e4376
Towhid Zaman <towhid1zaman@gmail.com>
2024-10-07 01:45:44 +0600

    add restart game option

--- Connect3Game/app/src/main/java/com/example/connect3game/MainActivity.java
+++ Connect3Game/app/src/main/java/com/example/connect3game/MainActivity.java
@@ -1,33 +1,36 @@
 package com.example.connect3game;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     // choose alternate color
     // yellow player = 0, red player = 1;
 
-    private int[] gameSet = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[] gameSet = {2, 2, 2, 2, 2, 2, 2, 2, 2};
     // winning positions, set same row, column and diagonal
-    private int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
-    private int player = 1;
+    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+    int player = 0;
+    boolean gameActive = true;
+    @SuppressLint("SetTextI18n")
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
 
         int tappedCounter = Integer.parseInt((counter.getTag().toString()));
         Log.i("dropIn",counter.getTag().toString());
 
-        if (gameSet[tappedCounter] == 2) {
+        if (gameSet[tappedCounter] == 2 && gameActive) {
             gameSet[tappedCounter] = player;
 
             counter.setTranslationY(-1500);
@@ -46,12 +49,41 @@ public class MainActivity extends AppCompatActivity {
                         gameSet[winningPosition[1]] == gameSet[winningPosition[2]] &&
                         gameSet[winningPosition[0]] != 2) {
                     // someone has won
-
+                    gameActive = false;
                     String winningPlayer = (player == 0) ? "Red" : "Yellow"; // reverse, always changed value after checked
-                    Toast.makeText(this, winningPlayer + " won the game", Toast.LENGTH_SHORT).show();
+
+                    Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+                    TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+
+                    winnerTextView.setText(winningPlayer + " has won!");
+                    winnerTextView.setVisibility(View.VISIBLE);
+                    playAgainButton.setVisibility(View.VISIBLE);
                 }
             }
+        } else if (gameActive) {
+            Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+            playAgainButton.setVisibility(View.VISIBLE);
         }
+    }
+
+    public void playAgain(View view) {
+        // reset all
+
+        Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+        TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+        winnerTextView.setVisibility(View.INVISIBLE);
+        playAgainButton.setVisibility(View.INVISIBLE);
+
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+        for(int i = 0; i < gridLayout.getChildCount(); i++) {
+            ImageView counter = (ImageView) gridLayout.getChildAt(i);
+            // do stuff with child view
+            counter.setImageDrawable(null);
+        }
+
+        Arrays.fill(gameSet, 2);
+        player = 0;
+        gameActive = true;
     }
 
     @Override
