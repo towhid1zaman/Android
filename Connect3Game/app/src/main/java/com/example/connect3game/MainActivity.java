314c74d0753ecbea21ddeb73c01b683c05d3efae
Towhid Zaman <towhid1zaman@gmail.com>
2024-10-07 00:27:08 +0600

    add winning state, identify winner

--- Connect3Game/app/src/main/java/com/example/connect3game/MainActivity.java
+++ Connect3Game/app/src/main/java/com/example/connect3game/MainActivity.java
@@ -27,25 +27,29 @@ public class MainActivity extends AppCompatActivity {
         int tappedCounter = Integer.parseInt((counter.getTag().toString()));
         Log.i("dropIn",counter.getTag().toString());
 
-        gameSet[tappedCounter] = player;
+        if (gameSet[tappedCounter] == 2) {
+            gameSet[tappedCounter] = player;
 
-        counter.setTranslationY(-1500);
-        if (player == 0) {
-            counter.setImageResource(R.drawable.yellow);
-            player = 1;
-        } else {
-            counter.setImageResource(R.drawable.red);
-            player = 0;
-        }
-        counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+            counter.setTranslationY(-1500);
+            if (player == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                player = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                player = 0;
+            }
+            counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
 
-        // after game play, check is there any three same or not, that is winning position
-        for (int[] winningPosition: winningPositions) {
-            if (gameSet[ winningPosition[0] ] == gameSet[ winningPosition[1] ] &&
-                    gameSet[ winningPosition[0] ] == gameSet[ winningPosition[2] ] &&
-                gameSet[ winningPosition[0] ] != 2) {
-                // someone win 
-                Toast.makeText(this, "Someone has won the game", Toast.LENGTH_SHORT).show();
+            // after game play, check is there any three same or not, that is winning position
+            for (int[] winningPosition : winningPositions) {
+                if (gameSet[winningPosition[0]] == gameSet[winningPosition[1]] &&
+                        gameSet[winningPosition[1]] == gameSet[winningPosition[2]] &&
+                        gameSet[winningPosition[0]] != 2) {
+                    // someone has won
+
+                    String winningPlayer = (player == 0) ? "Red" : "Yellow"; // reverse, always changed value after checked
+                    Toast.makeText(this, winningPlayer + " won the game", Toast.LENGTH_SHORT).show();
+                }
             }
         }
     }
