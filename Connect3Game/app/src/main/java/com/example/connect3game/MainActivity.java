f320a9b508468464612db50af4752824ebd018f4
Towhid Zaman <towhid1zaman@gmail.com>
2024-10-06 22:40:18 +0600

    animate a cell by alternate color, dropIn

--- Connect3Game/app/src/main/java/com/example/connect3game/MainActivity.java
+++ Connect3Game/app/src/main/java/com/example/connect3game/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.connect3game;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void dropIn(View view) {
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1500);
+        counter.setImageResource(R.drawable.yellow);
+        counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+
 }
