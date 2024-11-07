74b808a1f8b58bc8282380025daa04a8a590ef3a
Towhid Zaman <towhid1zaman@gmail.com>
2024-11-07 23:14:58 +0600

    Action bar remove

--- SomeToyProject/TwitterClone/app/build.gradle.kts
+++ SomeToyProject/TwitterClone/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "towhid1zaman.toyproject.twitterclone"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
