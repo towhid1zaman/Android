3fd0c9aa4dd12222adcc4c2bfce7ed61aa5bf5d6
Towhid Zaman <towhid1zaman@gmail.com>
2024-11-07 20:01:52 +0600

    Twitter Clone app initial commit

---
+++ SomeToyProject/TwitterClone/app/src/main/java/towhid1zaman/toyproject/twitterclone/MainActivity.kt
@@ -0,0 +1,11 @@
+package towhid1zaman.toyproject.twitterclone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
