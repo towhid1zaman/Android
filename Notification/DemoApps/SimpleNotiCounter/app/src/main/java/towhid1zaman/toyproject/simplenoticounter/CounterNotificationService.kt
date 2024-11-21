167957c3127d20a7f01c00901b56c369ea58865c
Towhid Zaman <towhid1zaman@gmail.com>
2024-11-21 21:57:40 +0600

    Counter the value

--- Notification/DemoApps/SimpleNotiCounter/app/src/main/java/towhid1zaman/toyproject/simplenoticounter/CounterNotificationService.kt
+++ Notification/DemoApps/SimpleNotiCounter/app/src/main/java/towhid1zaman/toyproject/simplenoticounter/CounterNotificationService.kt
@@ -1,13 +1,16 @@
 package towhid1zaman.toyproject.simplenoticounter
 
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import androidx.core.content.getSystemService
 
 
 class CounterNotificationService(private val context: Context) {
+    private val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
     fun showNotification(counter: Int) {
         val activityIntent = Intent(context, MainActivity::class.java)
@@ -15,6 +18,12 @@ class CounterNotificationService(private val context: Context) {
              context,
             1,
             activityIntent,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
+        )
+        val incremnetIntent = PendingIntent.getBroadcast(
+            context, 2, Intent(
+                context, CounterNotificationReceiver::class.java
+            ),
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
         )
         val notification = NotificationCompat.Builder(context, COUNTER_CHANNEL_ID)
@@ -25,8 +34,11 @@ class CounterNotificationService(private val context: Context) {
             .addAction(
                 R.drawable.baseline_baby_changing_station_24,
                 "Incremnet",
-                    
+                incremnetIntent
                 )
+            .build()
+
+        notificationManager.notify(1, notification)
     }
 
     companion object{
--- Notification/DemoApps/SimpleNotiCounter/app/src/main/java/towhid1zaman/toyproject/simplenoticounter/MainActivity.kt
+++ Notification/DemoApps/SimpleNotiCounter/app/src/main/java/towhid1zaman/toyproject/simplenoticounter/MainActivity.kt
@@ -3,11 +3,28 @@ package towhid1zaman.toyproject.simplenoticounter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import towhid1zaman.toyproject.simplenoticounter.ui.theme.SimpleNotiCounterTheme
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
 
+        val service = CounterNotificationService(applicationContext)
+        setContent {
+            SimpleNotiCounterTheme {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Button(onClick = {
+                        service.showNotification(Counter.value)
+                    }) {
+                        Text(text = "Show notification")
+                    }
+                }
+            }
         }
     }
 }
