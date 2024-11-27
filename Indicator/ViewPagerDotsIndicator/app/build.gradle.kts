c96255db0f2056d7dffb813518e0855576e7bf6c
Towhid Zaman <towhid1zaman@gmail.com>
2024-11-27 22:18:02 +0600

    ViewPagerDotsIndicator initial commit

---
+++ Indicator/ViewPagerDotsIndicator/app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "towhid1zaman.toyproject.viewpagerdotsindicator"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "towhid1zaman.toyproject.viewpagerdotsindicator"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.play.services.wearable)
+}
