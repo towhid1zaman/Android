894d0d7278abf94b514a1aa2c7632ec215c4e6b8
Towhid Zaman <towhid1zaman@gmail.com>
2024-12-10 23:59:04 +0600

    RecyclerViewExample activity

--- ViewPager/RecyclerView/RecyclerViewExample/app/src/main/java/towhid1zaman/toyproject/recyclerviewexample/MainActivity.java
+++ ViewPager/RecyclerView/RecyclerViewExample/app/src/main/java/towhid1zaman/toyproject/recyclerviewexample/MainActivity.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        RecyclerView recyclerView = findViewById(R.id.recyclerContact);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
