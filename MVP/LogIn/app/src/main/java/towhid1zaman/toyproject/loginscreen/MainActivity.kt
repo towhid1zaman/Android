1afcb7a948eb5be1271ceb7d984534454771c520
Towhid Zaman <towhid1zaman@gmail.com>
2025-02-01 15:46:36 +0600

    mvp

--- MVP/LogIn/app/src/main/java/towhid1zaman/toyproject/loginscreen/MainActivity.kt
+++ MVP/LogIn/app/src/main/java/towhid1zaman/toyproject/loginscreen/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import towhid1zaman.toyproject.loginscreen.presenter.LoginPresenter
 import towhid1zaman.toyproject.loginscreen.view.LoginView
@@ -37,11 +38,13 @@ class MainActivity : AppCompatActivity(), LoginView {
     }
 
     override fun onLoginSuccess(message: String) {
-        TODO("Not yet implemented")
+        tvMessage.text = message
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
     override fun onLoginError(message: String) {
-        TODO("Not yet implemented")
+        tvMessage.text = message
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
 }
