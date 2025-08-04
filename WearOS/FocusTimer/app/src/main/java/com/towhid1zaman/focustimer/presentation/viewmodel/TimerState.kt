a1c1c0f406dd90ec39c9d695dcb515b26a1c720e
Towhid Zaman <towhid1zaman@gmail.com>
2025-08-05 00:44:28 +0600

    Add TimerState data class to represent timer UI state

--- WearOS/FocusTimer/app/src/main/java/com/towhid1zaman/focustimer/presentation/viewmodel/TimerState.kt
+++ WearOS/FocusTimer/app/src/main/java/com/towhid1zaman/focustimer/presentation/viewmodel/TimerState.kt
@@ -1,4 +1,18 @@
 package com.towhid1zaman.focustimer.presentation.viewmodel
 
-class TimerState {
-}
+data class TimerState (
+    val isRunning: Boolean =  false,
+    val timeLeftMillis: Long = 25*60*100L, // default 25 minutes
+    val totalTimeMillis: Long = 25*60*1000L,
+    val isFinished: Boolean = false,
+    val isPaused: Boolean = false
+)
+/*
+Property and Purpose for better understanding, will delete later :)
+
+isRunning	    Whether the timer is actively counting,
+timeLeftMillis	Milliseconds left on the countdown,
+totalTimeMillis	Full time at the start (useful for percent progress),
+isFinished	    True when countdown reaches 0,
+isPaused	    Whether the user has paused the timer
+ */
