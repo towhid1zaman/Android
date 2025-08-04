696b2d1c26d4582786f82fdffc5486037ce067b3
Towhid Zaman <towhid1zaman@gmail.com>
2025-08-05 00:07:54 +0600

    Updated Sdk version

--- WearOS/FocusTimer/app/build.gradle.kts
+++ WearOS/FocusTimer/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.towhid1zaman.focustimer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.towhid1zaman.focustimer"
